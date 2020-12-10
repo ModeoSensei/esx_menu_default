8b1b247d50b2708311d591cb5f089a78f857a46a
ModeoSensei <57114130+ModeoSensei@users.noreply.github.com>
2020-12-10 19:55:51 +0100

    esx_menu_default 1.0

---
+++ README.md
@@ -0,0 +1,4 @@
+# esx_menu_default
+ESX Menu Default
+
+release BY MODEO (Modeo#7090)
---
+++ __resource.lua
@@ -0,0 +1,32 @@
+------ BY MODEO (Modeo#7090) ------
+
+resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+
+description 'ESX Menu Default, release by Modeo'
+
+version '1.0.4'
+
+client_scripts {
+	'@es_extended/client/wrapper.lua',
+	'client/main.lua'
+}
+
+ui_page {
+	'html/ui.html'
+}
+
+files {
+	'html/ui.html',
+	'html/css/app.css',
+	'html/js/mustache.min.js',
+	'html/js/app.js',
+	'html/fonts/pdown.ttf',
+	'html/fonts/bankgothic.ttf'
+}
+
+dependencies {
+	'es_extended'
+}
+
+
+------ release BY MODEO (Modeo#7090) ------
